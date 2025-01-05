5856093ac297696bbd18eab26c4f5b6a886e286a
Ismael Mesa <ismaelmesazyout@gmail.com>
2025-01-05 12:37:03 +0100

    feat: quitado cache

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -27,25 +27,6 @@ export const createNotification = async (notificationData) => {
       await updateNotificationSummary(createdNotification.userId, 1);
     }
 
-    // Solo almacenar en caché si la notificación es 'SEEN'
-    if (createdNotification.notificationStatus === 'SEEN') {
-      // **Actualizar la caché**: La caché debe reflejar la nueva notificación
-      const cacheKey = `notification:${createdNotification._id}`;
-      await redisClient.set(cacheKey, JSON.stringify(createdNotification), 'EX', CACHE_TTL);
-
-      // Si la notificación es para un usuario específico, también debes actualizar la caché para las notificaciones del usuario
-      const userCacheKey = `notifications:user:${createdNotification.userId}`;
-      const cachedUserNotifications = await redisClient.get(userCacheKey);
-      if (cachedUserNotifications) {
-        const notifications = JSON.parse(cachedUserNotifications);
-        notifications.push(createdNotification); // Agregar la nueva notificación al arreglo
-        await redisClient.set(userCacheKey, JSON.stringify(notifications), 'EX', CACHE_TTL);
-      } else {
-        // Si no existe en caché, entonces se crea el primer valor
-        await redisClient.set(userCacheKey, JSON.stringify([createdNotification]), 'EX', CACHE_TTL);
-      }
-    }
-
     return createdNotification;
   } catch (error) {
     console.error('Error creating notification:', error);
@@ -53,35 +34,13 @@ export const createNotification = async (notificationData) => {
   }
 };
 
-// Función para obtener notificación por ID, con caché primero
 export const getNotificationById = async (id) => {
   try {
-    const cacheKey = `notification:${id}`;
-
-    // Verificar si los datos están en la caché
-    const cachedNotification = await redisClient.get(cacheKey);
-
-    if (cachedNotification) {
-      console.log('Cache hit for notification:', id);
-      
-      // Si está en caché, retornamos los datos directamente de la caché
-      const notificationObject = JSON.parse(cachedNotification);
-      
-      // Convertir el objeto de caché a un objeto Mongoose si es necesario
-      const notification = new NotificationModel(notificationObject);
-      return notification;
-    }
-
-    // Si no está en la caché, realizamos la consulta a la base de datos
-    console.log('Cache miss for notification:', id);
     const notification = await NotificationModel.findById(id);
-    
+
     if (!notification) {
       throw new NotFoundError('Notification not found');
     }
-
-    // Guardamos la respuesta en la caché para futuras consultas
-    await redisClient.set(cacheKey, JSON.stringify(notification), 'EX', CACHE_TTL);
 
     return notification;
   } catch (error) {
@@ -92,35 +51,20 @@ export const getNotificationById = async (id) => {
   }
 };
 
-
 export const getNotificationByUserId = async (userId) => {
   try {
-    const cacheKey = `notifications:user:${userId}`;
-
-    // Verificar si las notificaciones están en la caché
-    const cachedNotifications = await redisClient.get(cacheKey);
-    
-    if (cachedNotifications) {
-      console.log('Cache hit for user notifications:', userId);
-      return JSON.parse(cachedNotifications); // Si están en la caché, devolverlas
-    }
-
-    console.log('Cache miss for user notifications:', userId);
-
-    // Si no están en la caché, obtenerlas de la base de datos
     const notifications = await NotificationModel.find({ userId });
+
     if (!notifications || notifications.length === 0) {
       throw new NotFoundError('Notifications not found for the specified userId');
     }
 
-    // Guardar las notificaciones en la caché
-    await redisClient.set(cacheKey, JSON.stringify(notifications), 'EX', CACHE_TTL);
     return notifications;
-
   } catch (error) {
     throw new NotFoundError('Error fetching notifications by userId', error);
   }
 };
+
 
 export const getAllNotifications = async () => {
   try {
